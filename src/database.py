441a6aad1f08590075774d103b5a973bf338d0d8
LiamTyler <tyler.liam7@gmail.com>
2016-11-28 15:20:20 -0600

    Finished Implementing the Database interface. Tested and seems to be working just fine. Not using proper testing, but seems fine.

--- src/database.py
+++ src/database.py
@@ -12,18 +12,18 @@ class Database:
         self.cur_ = None
         self.Init()
 
+    # Connect to the database
     def Init(self):
-        if (self.connection_ == None):
-            try:
-                params = "dbname='%s' user='%s' host='%s' password='%s'" % \
-                         (self.dbname_, self.user_, self.host_, self.password_)
-                self.connection_ = psycopg2.connect(params)
-                self.cur_ = self.connection_.cursor()
-            except:
-                print "Failed to connect to database"
-                return False
-        else:
-            print "Connection already present"
+        if (self.connection_ != None):
+            self.Close()
+        try:
+            params = "dbname='%s' user='%s' host='%s' password='%s'" % \
+                     (self.dbname_, self.user_, self.host_, self.password_)
+            self.connection_ = psycopg2.connect(params)
+            self.cur_ = self.connection_.cursor()
+        except:
+            print "Failed to connect to database"
+            return False
         return True
 
     # Rollback the current transaction so that we can proceed with other
@@ -42,8 +42,12 @@ class Database:
     # Return all user's that match the search. Should only be at most one,
     # since the username is a primary key
     def SearchForUser(self, username):
-        self.cur_.execute("SELECT * FROM Users WHERE username = '%s'" % username)
-        return self.cur_.fetchall()
+        try:
+            self.cur_.execute("SELECT * FROM Users WHERE username = '%s'" % username)
+            return self.cur_.fetchall()
+        except:
+            self.Reset()
+            return "Error"
 
     # Add a user to the Users table, as long as they don't already exist
     def AddNewUser(self, username):
@@ -70,14 +74,23 @@ class Database:
                      AND friend='%s';" % (sending_user, receiving_user))
             if self.cur_.fetchall():
                 return "Already Friends"
-            self.cur_.execute("INSERT INTO FriendRequests (from_user, to_user)\
-                    values ('%s', '%s');" % (sending_user, receiving_user))
-            self.connection_.commit()
+            # If the receiving user already sent a friend request to the
+            # now sending user, instead of sending the request, just make them
+            # friends in each other's friends list
+            self.cur_.execute("SELECT * FROM FriendRequests WHERE from_user =\
+                    '%s' AND to_user = '%s';" % (receiving_user, sending_user))
+            if self.cur_.fetchall():
+                self.AcceptFriendRequest(sending_user, receiving_user)
+            else:
+                self.cur_.execute("INSERT INTO FriendRequests (from_user,\
+                        to_user) values ('%s', '%s');" % (sending_user,
+                         receiving_user))
+                self.connection_.commit()
             return "Success"
         except psycopg2.IntegrityError as e:
             self.Reset()
             if "already exists" in e.message:
-                return "Duplicate"
+                return "Duplicate request"
             elif "is not present" in e.message:
                 return "Invalid User"
             else:
@@ -90,14 +103,8 @@ class Database:
     # sending_user to the receiving_user's friend list, and visa versa
     def AcceptFriendRequest(self, sending_user, receiving_user):
         try:
-            self.cur_.execute("SELECT * FROM FriendRequests WHERE from_user='%s'\
-                     AND to_user='%s';" % (sending_user, receiving_user))
-            if not self.cur_.fetchall():
-                return "No request to accept"
             self.cur_.execute("DELETE FROM FriendRequests WHERE from_user='%s'\
                      AND to_user='%s';" % (sending_user, receiving_user))
-            # Don't think there should be reverse relation, but if there is,
-            # definitely don't want it there anymore
             self.cur_.execute("DELETE FROM FriendRequests WHERE from_user='%s'\
                      AND to_user='%s';" % (receiving_user, sending_user))
             self.cur_.execute("INSERT INTO FriendsList (username, friend)\
@@ -105,17 +112,80 @@ class Database:
             self.cur_.execute("INSERT INTO FriendsList (username, friend)\
                     values ('%s', '%s');" % (receiving_user, sending_user))
             self.connection_.commit()
+            return "Success"
         except:
             self.Reset()
-            return 
+            return "Error"
+    
+    # Remove the friend request sent by sending_user, and received
+    # by the receiving_user
+    def RejectFriendRequest(self, sending_user, receiving_user):
+        try:
+            self.cur_.execute("DELETE FROM FriendRequests WHERE from_user='%s'\
+                     AND to_user='%s';" % (sending_user, receiving_user))
+            self.connection_.commit()
+            return "Success"
+        except:
+            self.Reset()
+            return "Error"
 
     def ViewFriendRequests(self, username):
-        self.cur_.execute("SELECT from_user FROM FriendRequests WHERE to_user='%s';" %
-                (username,))
-        return self.cur_.fetchall()
+        try:
+            self.cur_.execute("SELECT from_user FROM FriendRequests WHERE to_user='%s';" %
+                    (username,))
+            return self.cur_.fetchall()
+        except:
+            self.Reset()
+            return "Error"
 
     def ViewFriendsList(self, username):
-        self.cur_.execute("SELECT friend FROM FriendsList WHERE username='%s';" %
-                (username,))
-        return self.cur_.fetchall()
+        try:
+            self.cur_.execute("SELECT friend FROM FriendsList WHERE username=\
+                    '%s';" % username)
+            return self.cur_.fetchall()
+        except:
+            self.Reset()
+            return "Error"
 
+    def RemoveFriend(self, username, friend):
+        try:
+            self.cur_.execute("DELETE FROM FriendsList WHERE username='%s' AND\
+                    friend='%s';" % (username, friend))
+            # Remove the reverse too
+            self.cur_.execute("DELETE FROM FriendsList WHERE username='%s' AND\
+                    friend='%s';" % (friend, username))
+            self.connection_.commit()
+            return "Success"
+        except:
+            self.Reset()
+            return "Error"
+
+    def RemoveUser(self, username):
+        try:
+            self.cur_.execute("DELETE FROM Users WHERE username='%s';" %
+                              username)
+            self.connection_.commit()
+            return "Success"
+        except:
+            self.Reset()
+            return "Error"
+
+    def SetOnlineStatus(self, username, status):
+        try:
+            self.cur_.execute("UPDATE Users SET onlineStatus = '%s' WHERE \
+                               username = '%s';" % (str(status), username))
+            self.connection_.commit()
+            return "Success"
+        except:
+            self.Reset()
+            return "Error"
+    
+    def SetInGameStatus(self, username, status):
+        try:
+            self.cur_.execute("UPDATE Users SET inGameStatus = '%s' WHERE \
+                               username = '%s';" % (str(status), username))
+            self.connection_.commit()
+            return "Success"
+        except:
+            self.Reset()
+            return "Error"
